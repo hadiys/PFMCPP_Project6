b5e77369abddd08641c6384b320d7c2e6149b7e2
Hadi Saleh <66527346+hadiys@users.noreply.github.com>
2021-12-30 20:58:06 +0000

    Changed all function args from ptrs to refsa and made relevant changes to the code.

--- main.cpp
+++ main.cpp
@@ -35,13 +35,11 @@ struct T
 
 struct V                                //4
 {
-    T* compare(T* a, T* b) //5
+    T* compare(T& a, T& b) //5
     {
-        if(a != nullptr && b != nullptr)
-        {
-            if( a->value < b->value ) return a;
-            if( a->value > b->value ) return b;
-        }
+    
+        if( a.value < b.value ) return &a;
+        if( a.value > b.value ) return &b;
 
         return nullptr;
     }
@@ -50,51 +48,40 @@ struct V                                //4
 struct U
 {
     float f1 { 0 }, f2 { 0 };
-    float reduceDistance(float* newValue)      //12
+    float reduceDistance(float& newValue)      //12
     {
-        if(newValue != nullptr)
+        std::cout << "U's f1 value: " << this->f1 << std::endl;
+        this->f1 = newValue;
+        std::cout << "U's f1 updated value: " << this->f1 << std::endl;
+        while( std::abs(this->f2 - this->f1) > 0.001f )
         {
-            std::cout << "U's f1 value: " << this->f1 << std::endl;
-            this->f1 = *newValue;
-            std::cout << "U's f1 updated value: " << this->f1 << std::endl;
-            while( std::abs(this->f2 - this->f1) > 0.001f )
-            {
-                /*
-                write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
-                */
-                this->f2 += 0.1f;
-            }
-            std::cout << "U's f2 updated value: " << this->f2 << std::endl;
-            return this->f2 * this->f1;
-            }
-
-            std::cout << "ERROR: nullptr" << std::endl;
-            return 0.f;
+            /*
+            write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
+            */
+            this->f2 += 0.1f;
+        }
+        std::cout << "U's f2 updated value: " << this->f2 << std::endl;
+        return this->f2 * this->f1;
     }
 };
 
 struct StaticFunc
 {
-    static float reduceDistanceOfU(U* that, float* newValue )        //10
+    static float reduceDistanceOfU(U& that, float& newValue )        //10
     {
-        if(that != nullptr && newValue != nullptr)
-        {
-            std::cout << "U's f1 value: " << that->f1 << std::endl;
-            that->f1 = *newValue;
-            std::cout << "U's f1 updated value: " << that->f1 << std::endl;
-            while( std::abs(that->f2 - that->f1) > 0.001f )
-            {
-                /*
-                write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
-                */
-                that->f2 += 0.1f;
-            }
-            std::cout << "U's f2 updated value: " << that->f2 << std::endl;
-            return that->f2 * that->f1;
-        }
         
-        std::cout << "ERROR: nullptr" << std::endl;
-        return 0.f;
+        std::cout << "U's f1 value: " << that.f1 << std::endl;
+        that.f1 = newValue;
+        std::cout << "U's f1 updated value: " << that.f1 << std::endl;
+        while( std::abs(that.f2 - that.f1) > 0.001f )
+        {
+            /*
+            write something that makes the distance between that-><#name2#> and that-><#name1#> get smaller
+            */
+            that.f2 += 0.1f;
+        }
+        std::cout << "U's f2 updated value: " << that.f2 << std::endl;
+        return that.f2 * that.f1;
     }
 };
         
@@ -118,14 +105,16 @@ int main()
     T t2(8.7, "t2");                                             //6
     
     V f;                                            //7
-    auto* smaller = f.compare(&t1, &t2);                              //8
+    auto* smaller = f.compare(t1, t2);                              //8
     if(smaller != nullptr) 
         std::cout << "the smaller one is << " << smaller->name << std::endl; //9
-    
+    else
+        std::cout << "Returned nullptr since t1 == t2" << std::endl; //9
+
     U u1;
     float updatedValue = 5.f;
-    std::cout << "reduceDistanceOfU() u1's multiplied values: " << StaticFunc::reduceDistanceOfU(&u1, &updatedValue) << std::endl;                  //11
+    std::cout << "reduceDistanceOfU() u1's multiplied values: " << StaticFunc::reduceDistanceOfU(u1, updatedValue) << std::endl;                  //11
     
     U u2;
-    std::cout << "reduceDistance() u2's multiplied values: " << u2.reduceDistance(&updatedValue) << std::endl;
+    std::cout << "reduceDistance() u2's multiplied values: " << u2.reduceDistance(updatedValue) << std::endl;
 }
